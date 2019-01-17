c29ee74fe60f349579c0a69e662d715771ad36d1
thayllananacleto <thayllananacleto@gmail.com>
2019-01-17 00:12:46 -0500

    Initial Commit

---
+++ SlothMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlothMachine
+//
+//  Created by Thayllan Anacleto on 2019-01-17.
+//  Copyright © 2019 Thayllan Anacleto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
